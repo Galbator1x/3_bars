4acf63699f6ebe78deb546a8a642cf971e58870f
Ivan Filimonov <snake73w@gmail.com>
2016-09-30 01:24:27 +0700

    refactored all functions.

--- bars.py
+++ bars.py
@@ -10,11 +10,11 @@ def get_filepath_from_argv(argv):
     try:
         filepath = argv[1]
     except IndexError as error:
-        print('Введите путь к файлу Бары.json первым параметром')
+        print('Enter the path to the list of bars by first parameter')
         sys.exit()
     else:
         if not os.path.exists(filepath):
-            print('Файл не существует')
+            print('File does not exists.')
             sys.exit()
     return filepath
 
@@ -25,54 +25,27 @@ def load_data(filepath):
 
 
 def get_biggest_bar(data):
-    biggest_bars = []
-    max_seats = -1
-    for bar in data:
-        seats_count = bar['Cells']['SeatsCount']
-        if seats_count > max_seats:
-            max_seats = seats_count
-            biggest_bars.clear()
-            biggest_bars.append(bar['Cells']['Name'])
-        elif seats_count == max_seats:
-            biggest_bars.append(bar['Cells']['Name'])
-    return biggest_bars
+    biggest_bar = max(data, key=lambda x: x['Cells']['SeatsCount'])
+    max_seats = biggest_bar['Cells']['SeatsCount']
+    return [bar['Cells']['Name'] for bar in data if bar['Cells']['SeatsCount'] == max_seats]
 
 
 def get_smallest_bar(data):
-    smallest_bars = []
-    min_seats = 1000000
-    for bar in data:
-        seats_count = bar['Cells']['SeatsCount']
-        if seats_count < min_seats:
-            min_seats = seats_count
-            smallest_bars.clear()
-            smallest_bars.append(bar['Cells']['Name'])
-        elif seats_count == min_seats:
-            smallest_bars.append(bar['Cells']['Name'])
-    return smallest_bars
-
-
-def get_distance_to_bar(longitude1, longitude2, latitude1, latitude2):
-    return sqrt((longitude1 - longitude2) ** 2 + (latitude1 - latitude2) ** 2)
+    smallest_bar = min(data, key=lambda x: x['Cells']['SeatsCount'])
+    min_seats = smallest_bar['Cells']['SeatsCount']
+    return [bar['Cells']['Name'] for bar in data if bar['Cells']['SeatsCount'] == min_seats]
 
 
 def get_closest_bar(data, longitude, latitude):
-    closest_bar = None
-    min_distance = 1000000
-    for bar in data:
-        coordinates = bar['Cells']['geoData']['coordinates']
-        longitude2, latitude2 = coordinates[0], coordinates[1]
-        distance = get_distance_to_bar(longitude, longitude2, latitude, latitude2)
-        if distance < min_distance:
-            min_distance = distance
-            closest_bar = bar['Cells']['Name']
-    return closest_bar
+    closest_bar = min(data, key=lambda x: sqrt((longitude - x['Cells']['geoData']['coordinates'][0]) ** 2 + (
+        latitude - x['Cells']['geoData']['coordinates'][1]) ** 2))
+    return closest_bar['Cells']['Name']
 
 
 if __name__ == '__main__':
     filepath = get_filepath_from_argv(argv)
     data = load_data(filepath)
-    longitude = float(input('Введите координаты\nlongitude:'))
+    longitude = float(input('Enter the coordinates\nlongitude:'))
     latitude = float(input('latitude:'))
 
     print('Biggest bar: {}'.format(get_biggest_bar(data)))
